0786d0990a899a47b26c16b178d0400514cb63ea
AnkitIITR <ankit_y@ar.iitr.ac.in>
2024-05-13 10:20:48 +0530

    update the upload-thing

--- first-t3/src/app/_components/navbar.tsx
+++ first-t3/src/app/_components/navbar.tsx
@@ -1,15 +1,20 @@
+"use client"
+import { useRouter } from "next/navigation";
 import { SignedOut,SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "~/utils/uploadthing";
 
 export default function Navbar() {
+  const router=useRouter();
   return (
     <div className="flex justify-between px-10 py-5">
       <div>Gallery</div>
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
           <UserButton />
+          <UploadButton endpoint="imageUploader" onClientUploadComplete={()=>{router.refresh()}}/>
         </SignedIn>
       </div>
     </div>
