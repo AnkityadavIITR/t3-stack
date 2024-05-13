4d8e4495e3c901f6836d1ffee9e8ab03a972ecba
AnkitIITR <ankit_y@ar.iitr.ac.in>
2024-05-13 08:35:49 +0530

    updates on db

--- first-t3/src/app/page.tsx
+++ first-t3/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import {db} from "./../server/db/index"
+import Image from "next/image";
 
 import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/server";
@@ -7,14 +8,19 @@ import { api } from "~/trpc/server";
 export const dynamic="force-dynamic"
 
 export default async function Home() {
-  const posts = await db.query.posts.findMany();
+  const posts = await db.query.posts.findMany({
+    orderBy:(model,{desc})=>desc(model.id)
+  });
   console.log(posts)
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-wrap gap-4 bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       {
         posts?.map((post)=>{
-          return <h1 key={post.id}>{post.name}</h1>
+          return <div key={post.id}>
+            <img src={post.url} alt="post" className="w-[200px] h-[200px]"></img>
+            <p>{post.name}</p>
+          </div>
         })
       }
     </main>
